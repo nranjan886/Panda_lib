540306cdefa666659e948d9d9a96c6fe203d61da
Nitesh Ranjan <nranjan886@gmail.com>
2024-04-05 09:38:40 +0530

    Added read-data route

--- routes/panda.py
+++ routes/panda.py
@@ -2,7 +2,7 @@ from urllib import request
 import config.MongoConfig
 import pandas as pd
 
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 
 PANDA_LIB = Blueprint("PANDA_LIB", __name__)
 
@@ -21,6 +21,22 @@ def upload_file():
         collection.insert_many(json_data)
         conn.close()
         return "Data Updated successfully in mongoDB!!!"
+    except Exception as e:
+        print(e)
+
+
+@PANDA_LIB.route("/read-data", methods=['GET'])
+def read_data():
+    try:
+        conn = config.MongoConfig.get_connect()
+        db = conn['my_db']
+        collection = db["my_collection"]
+        data = list(collection.find({}))  # Fetch all documents from the collection
+        conn.close()
+        df = pd.DataFrame(data)
+        if '_id' in df.columns:
+            df.drop('_id', axis=1, inplace=True)
+        return df.to_json(orient='records')
     except Exception as e:
         print(e)
 
